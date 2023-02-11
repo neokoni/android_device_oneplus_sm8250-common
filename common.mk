0da2edc1116f2c73052d8ad2342ef5de16fa1af5
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    sm8250-common: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: Mathieu Chartier <mathieuc@google.com>
    Signed-off-by: Quince <quinceroms@gmail.com>
    Signed-off-by: Codecity001 <sanganiharish263@gmail.com>

--- common.mk
+++ common.mk
@@ -111,6 +111,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 USE_DEX2OAT_DEBUG := false
 
