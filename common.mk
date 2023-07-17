f6b1b706ba3af3645217d56b0c100032317d87e3
ghostrider-reborn <adithya.r02@outlook.com>
2023-07-17 17:29:27 +0800

    sm8250-common: device: Optimize ART
    * Disable ART debug and optimize dexpreopt
    * Disable dexpreopt minidebuginfo
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- common.mk
+++ common.mk
@@ -109,11 +109,8 @@ $(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservic
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-USE_DEX2OAT_DEBUG := false
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
