9ef203495d3967e015fd6a6e7e74b18229a1262b
pjgowtham <pjgowtham@gmail.com>
2024-10-04 19:25:52 +0530

    sm8250-common: Switch to oplus sensors AIDL
    Change-Id: Ibe50667179a42845918f84bc403da21d2c28b160

--- common.mk
+++ common.mk
@@ -272,7 +272,7 @@ $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_en
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal \
+    android.hardware.sensors-service.oplus-multihal \
     sensors.oplus
 
 PRODUCT_COPY_FILES += \
