69e64ab7c5b69484ae0c34fbe74005cc3a017ac5
bojantanasic <bojan.tanasic@gmail.com>
2026-05-24 18:28:38 +0200

    sm8250-common: Add init.qcom.power.rc for libperfmgr

--- common.mk
+++ common.mk
@@ -195,6 +195,7 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
