a7f58d253f1e8dd484568463c58dbec1f6e65aa4
neokoni <dulin081682@126.com>
2026-06-19 13:33:51 +0800

    sm8250-common: don't build OplusDoze
    Signed-off-by: neokoni <dulin081682@126.com>

--- common.mk
+++ common.mk
@@ -127,9 +127,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # Doze
-PRODUCT_PACKAGES += \
-    OplusDoze \
-    OplusDozeResCommon
+# PRODUCT_PACKAGES += \
+#     OplusDoze \
+#     OplusDozeResCommon
 
 # DRM
 PRODUCT_PACKAGES += \
